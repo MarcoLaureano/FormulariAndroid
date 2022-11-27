4f464b6d9dba3aaa5794fdecb4de2f8ac2ac94fa
MarcoLaureano <mlaurean19@jaumebalmes.net>
2022-11-27 16:46:59 +0100

    "FormulariMarcoLaureano"

---
+++ app/src/main/java/com/example/formulario/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.formulario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.Spannable;
+import android.text.style.StyleSpan;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, SubmitedFoorm.class));
+            }
+        });
+
+    }
+}
